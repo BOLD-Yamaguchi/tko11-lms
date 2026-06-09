4405301da078f75fe84db71f7ccd3bf66970289f
Kubota Kazuya <bold.tko11@gmail.com>
2026-06-09 15:00:53 +0900

    2026/06/09 久保田 フォルダ構成見直し。

---
+++ backend/src/main/java/com/example/demo/tko11_lmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class tko11_lmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(tko11_lmsApplication.class, args);
+	}
+
+}
